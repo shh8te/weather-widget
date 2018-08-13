3431b3596be46c461010b7fa4d052e76cc44279e
shh8te <shh8te@gmail.com>
2018-08-13 19:40:48 +0300

    global-styles fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM, { render } from 'react-dom'
-import './global-styles'
+import './global-styles.css'
 import Root from './components/Root'
 
 render(<Root />, document.getElementById('root'))
